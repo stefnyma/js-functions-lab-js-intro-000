0a1b385338e54b973566d0563eafea7a6c2aaf30
Stephanie Ma <stephanieoma@gmail.com>
2019-06-16 23:00:02 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // write your code below!
+function happyHolidays() {
+  return "Happy holidays!"
+}
+
+function happyHolidaysTo(Hunter) {
+  return "Happy holidays, Hunter!"
+}
+
+function happyHolidayTo(Christmas, Chase) {
+  return "Happy holidays, Chase!"
+}
+
+function holidayCountdown(July Fourth, 18) {
+  return "It's 18 days until July Fourth!"
+}
